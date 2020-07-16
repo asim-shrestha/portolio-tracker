43355ec921b127485442e473d9a5219b72e7b94a
asim-shrestha <asim.shrestha@hotmail.com>
2020-07-16 14:44:26 -0700

    refactor: turn class based components into functional components

--- src/views/App.js
+++ src/views/App.js
@@ -1,23 +1,17 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Home from './components/Home';
 import Login from './components/Auth/Login';
-import { Switch, Route, withRouter } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 
-class App extends Component {
-    constructor(props){
-        super(props);
-    }    
-
-    render() {
-        return (
-            <Switch>
-                <Route exact path="/" render={() => <Home />}/>
-                <Route path="/login" render={() => <Login />}/>
-            </Switch>
-        );
-    }
+const App = () => {
+    return (
+        <Switch>
+            <Route exact path="/" render={() => <Home />}/>
+            <Route path="/login" render={() => <Login />}/>
+        </Switch>
+    );
 }
 
-export default withRouter(App);
+export default App;
 
