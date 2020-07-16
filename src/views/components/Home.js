5117ee92936fc1c34c53b7dd68589305c0f1377e
Tse-lun Chiu <tsc8@sfu.ca>
2020-07-16 10:16:40 -0700

    setup home page

--- src/views/components/Home.js
+++ src/views/components/Home.js
@@ -1,26 +1,38 @@
 import React, { Component } from 'react';
-import Typography from '@material-ui/core/Typography';
+// import Typography from '@material-ui/core/Typography';
 import homeStyles from '../../../public/style/homeStyles';
 import { withStyles } from '@material-ui/core/styles';
 import { withRouter } from "react-router-dom";
 import { render } from 'react-dom';
-import Test from './Activities/Buy';
+import { TextField, Typography, Button, ButtonGroup } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
-
+const useStyles = makeStyles((theme) => ({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    }
+}));
 
 class Home extends Component {
     constructor(props){
         super(props);
     }    
-
+    
     render(){
         const { classes } = this.props
 
         return (
-            <Typography className={classes.root} color="textSecondary">
-                App Skeleton:
-                Powered by React and Express.
-                <Test />
+                <Typography variant="h3" color="secondary" align="center">
+                    <br />
+                    <div>{"Welcome to "}</div>
+                    <div>{"Portfolio Tracker 1.0"}</div>
+                <ButtonGroup color="secondary" aria-label="outlined primary button group">
+                    <Button onClick={()=>{loginHandler;}}> Login </Button>
+                    <Button onClick={()=>{createHandler;}}> Create Account </Button>
+                </ButtonGroup>
             </Typography>
         );
     }
---
+++ src/views/components/List.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+
+
+class List extends Component {
+    render() {
+
+        return (
+            <div>placeholder for list</div>
+        )
+    }
+}
+
+export default List;
