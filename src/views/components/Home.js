604e983df3f90c62a6908c1bff41b4c78bc9101b
Tse-lun Chiu <tsc8@sfu.ca>
2020-07-17 01:21:20 -0700

    feat: add table: gitlab dead lol

--- src/views/components/Home.js
+++ src/views/components/Home.js
@@ -4,8 +4,10 @@ import homeStyles from '../../../public/style/homeStyles';
 import { withStyles } from '@material-ui/core/styles';
 import { withRouter } from "react-router-dom";
 import { Typography, Button, ButtonGroup } from '@material-ui/core';
+import DrawHome from './Draw/DrawHome';
 import DrawLine from './Draw/DrawLine';
 import DrawPie from './Draw/DrawPie';
+import DrawTable from './Draw/DrawTable';
 
 class Home extends Component {
     constructor(props){
@@ -13,7 +15,7 @@ class Home extends Component {
     }    
     render(){
         return (
-            <Typography variant="h1" color="secondary" align="center">
+            <Typography variant="h1" color="secondary" alignCenter>
                 <br /><br />
                 <div>{"Welcome to "}</div>
                 <div>{"Portfolio Tracker"}</div>
@@ -21,10 +23,13 @@ class Home extends Component {
                     <Button onClick={()=>{loginHandler;}}> Login </Button>
                     <Button onClick={()=>{createHandler;}}> Create Account </Button>
                 </ButtonGroup>
-                <div>
+
+                <DrawHome />
                 <DrawLine />
                 <DrawPie />
-                </div>
+                <DrawTable />
+
+
             </Typography>
         );
     }
