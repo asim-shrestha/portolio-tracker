8cb59605450ee1bb99066525c5428a04c7707b8e
Joe Shen <joe_shen@sfu.ca>
2020-07-18 14:46:19 -0700

    feat: refactor helper function validateSymbol

--- src/controllers/helpers/ActivitiesHelper.js
+++ src/controllers/helpers/ActivitiesHelper.js
@@ -1,25 +1,9 @@
 import { quote, iexSymbols } from 'iexcloud_api_wrapper'
 
 export default class ActivitiesHelper { 
-    generateSymbols(iexSymbols) {
-        let symbols = [];
-        const length = iexSymbols.length;
-        
-        for (let i = 0; i < length; i++ ){
-            symbols[i] = iexSymbols[i].symbol
-        }
-        return symbols
-    }
-
-    validateSymbol(sym, iexSymbols) {
+    validateSymbol(symbol, iexSymbols) {
         return new Promise((resolve, reject) => {
-            const symbols = this.generateSymbols(iexSymbols)
-
-            if (symbols.includes(sym)) {
-                resolve(true)
-            } else {
-                resolve(false)
-            }
+            resolve(iexSymbols.find(element => element.symbol == symbol) ? true : false)
         })
     }
 }
