e6c97420b38f908142b0de3c8821e4d5f5381e7f
Asim Shrestha <asim.shrestha@hotmail.com>
2020-07-21 00:10:36 -0700

    refactor: remove logs

--- app/src/views/components/Auth/UserStore.js
+++ app/src/views/components/Auth/UserStore.js
@@ -4,8 +4,6 @@ export const UserContext = createContext();
 
 export const UserProvider = ({children}) => {
     const [user, setUser] = useState(null);
-    console.log("Current user:");
-    console.log(user);
     return (
         <UserContext.Provider value={[user, setUser]}>
             {children}
