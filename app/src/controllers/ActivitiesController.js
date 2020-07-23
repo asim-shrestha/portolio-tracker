447490c425683c09b87824541fe1cbaa9685cb46
Tse-lun Chiu <tsc8@sfu.ca>
2020-07-23 16:45:26 -0700

    buy sell popup

--- app/src/controllers/ActivitiesController.js
+++ app/src/controllers/ActivitiesController.js
@@ -6,17 +6,18 @@ export default class ActivitiesController {
     // POST /activity/sell/:id
     // insert stock holding info (after purchase)
     async insertNewActivity(req, res) {
+        // console.log("insertNewActivity");
         try {
             // TODO: remove req.body after form is correctly setup
-            req.body = {
-                user_id: 1,
-                quantity: 120,
-                action:'buy',
-                symbol:'AMZN',
-                price: 3150.23,
-                date:'2020-07-17',
-                commission: 9.95
-            }
+            // req.body = {
+            //     user_id: 1,
+            //     quantity: 120,
+            //     action:'buy',
+            //     symbol:'AMZN',
+            //     price: 3150.23,
+            //     date:'2020-07-17',
+            //     commission: 9.95
+            // }
             
             const newActivity = await Activity.query().insert(req.body)
             res.json(newActivity);
@@ -28,6 +29,8 @@ export default class ActivitiesController {
     // POST /upload
     // upload user's csv file
     async uploadCSV(req, res) {
+        console.log("uploadCSV");
+
         try {
             res.send('NOT IMPLEMENTED: insert stock holding info (after purchase)');
         } catch(err) {
--- app/src/views/components/Holdings/AddHoldingsDialog.js
+++ app/src/views/components/Holdings/AddHoldingsDialog.js
@@ -6,29 +6,44 @@ import AppDialog from '../AppDialog';
 
 export default ({open, onClose}) => {
     const [user, setUser] = useContext(UserContext);
-    const [stock, setStock] = useState({
-        symbol: '',
-        price: '',
-        quantity: '',
-        date: '',
-        commission: '',
-        action: '',
-    })
+    const [symbol, setSymbol] = useState();
+    const [price, setPrice] = useState();
+    const [quantity, setQuantity] = useState();
+    const [date, setDate] = useState();
+    const [commission, setCommission] = useState();
+    const [action, setAction] = useState();
 
-    const updateStock= (attribute, value) => {
-        updatedStock = {...stock};
-        updateStock[attribute] = value;
-        setStock(updateStock);
+    // const updateStock= (attribute, value) => {
+    //     updatedStock = {...stock};
+    //     updateStock[attribute] = value;
+    //     setStock(updateStock);
+    // }
+
+    const onAdd = () => {
+        console.log(user.id);
+        Axios.post('/api/activity/order', {
+            user_id:user.id,
+            quantity:parseInt(quantity),
+            action:action,
+            symbol:symbol,
+            price:parseFloat(price),
+            date:date,
+            commission: parseFloat(commission)
+        }).then((res) => {
+            console.log(res);
+        }).catch((err) => {
+            alert(err);
+        })
     }
 
     return (
-        <AppDialog open={open} onClose={onClose} title={"Add Individual Holding"} buttonClick={onClose} buttonText={"Add (Not Implemented)"}>
-            <TextField variant="outlined" value={stock.symbol} onChange={e => updateStock('symbol', e.target.value)} fullWidth placeholder="Symbol"/>
-            <TextField variant="outlined" value={stock.price} onChange={e => updateStock('price', e.target.value)} fullWidth placeholder="Price"/>
-            <TextField variant="outlined" value={stock.quantity} onChange={e => updateStock('quantity', e.target.value)} fullWidth placeholder="Quantity"/>
-            <TextField variant="outlined" value={stock.date} onChange={e => updateStock('date', e.target.value)} fullWidth placeholder="Date"/>
-            <TextField variant="outlined" value={stock.commision} onChange={e => updateStock('commission', e.target.value)} fullWidth placeholder="Commission"/>
-            <TextField variant="outlined" value={stock.action} onChange={e => updateStock('action', e.target.value)} fullWidth placeholder="Action"/>
+        <AppDialog open={open} onClose={onClose} title={"Add Individual Holding"} buttonClick={onAdd} buttonText={"Add (Somewhat Implemented)"}>
+            <TextField variant="outlined" onChange={e => setSymbol(e.target.value)} fullWidth placeholder="Symbol"/>
+            <TextField variant="outlined" onChange={e => setPrice(e.target.value)} fullWidth placeholder="Price"/>
+            <TextField variant="outlined" onChange={e => setQuantity(e.target.value)} fullWidth placeholder="Quantity"/>
+            <TextField variant="outlined" onChange={e => setDate(e.target.value)} fullWidth placeholder="Date"/>
+            <TextField variant="outlined" onChange={e => setCommission(e.target.value)} fullWidth placeholder="Commission"/>
+            <TextField variant="outlined" onChange={e => setAction(e.target.value)} fullWidth placeholder="Action"/>
         </AppDialog>
     )
 }
