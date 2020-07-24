d2fd85599102dbcf927f45854a625b442391ed88
Tse-lun Chiu <tsc8@sfu.ca>
2020-07-24 12:23:46 -0700

    33 move close to top right corner

--- app/src/views/components/AppDialog.js
+++ app/src/views/components/AppDialog.js
@@ -5,20 +5,25 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import Button from '@material-ui/core/Button';
 import theme from '../../../public/style/theme';
+import {IconButton} from "@material-ui/core";
 
 // A generic dialog component
 // Always contains a close button
 // Has one additional button which you can provide the text and action for
 export default ({open, onClose, title, children, buttonClick, buttonText}) => {
+    let closeButton = {color:'white', cursor:'pointer', float:'right', margin: '0px', padding: '0px', width: '20px'};
+
     return (
         <Dialog open={open} onClose={onClose}>
-            <DialogTitle style={{backgroundColor: theme.palette.primary.main, color: "white"}}>{title}</DialogTitle>
+            <DialogTitle style={{backgroundColor: theme.palette.primary.main, color: "white"}}> {title}
+                <IconButton color="secondary" onClick={onClose} style={closeButton}>x</IconButton>
+            </DialogTitle>
+                
             <DialogContent style={{marginTop: "1em"}}>
                 {children}
             </DialogContent>
             <DialogActions>
                 <Button variant="contained" color="primary" onClick={buttonClick}>{buttonText}</Button>
-                <Button variant="contained" color="primary" onClick={onClose}>Close</Button>
             </DialogActions>
         </Dialog>
     )
