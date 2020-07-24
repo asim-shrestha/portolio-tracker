b310d1b9d277a327777b519d351e7478f1e1dce8
Asim Shrestha <asim.shrestha@hotmail.com>
2020-07-24 12:07:08 -0700

    feat: fix comments

--- app/src/views/components/Draw/PreformanceGraph.js
+++ app/src/views/components/Draw/PreformanceGraph.js
@@ -1,25 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import {
-    AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip,
-} from 'recharts';
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
 import theme from '../../../../public/style/theme';
 import Typography from '@material-ui/core/Typography';
 
 // TODO share method with HoldingsTable
 // Format currency related strings to include commas and only two decimal places
 const currencyFormat = (n) => {
-    if(n === null) {return 0};
+    if (n === null) { return 0 };
     return n.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
 }
 
 // Graph of preformance data that resizes alongside the window
-// Will return the preformance graph if data is given
-// Will otherwise return a message saying the portfolio is currently empty
+// Returns the graph if data is given and a message saying "the portfolio is currently empty" otherwise
 const PreformanceGraph = ({ data }) => {
     const [width, setWidth] = useState(0);
     const color = theme.palette.primary.main;
 
-    // Update width so that the graph is centered with margin on the right
     const updateWidth = () => {
         // Subtract magic number to get proper right margins
         setWidth(window.innerWidth - theme.spacing(28) * 2);
@@ -35,15 +31,13 @@ const PreformanceGraph = ({ data }) => {
         }
     }, [])
 
-    // Calculate total cash in portfolio
-    let totalCash = 0;
-    if(data.length > 0) {totalCash = data[data.length - 1].y;}
+    let totalPortfolioCash = 0;
+    if (data.length > 0) { totalPortfolioCash = data[data.length - 1].y; } // Ammount at today's date
 
-    // Only render if data is available and width !- 0
-    // This ensures that the graph will be animated when loaded
+    // This loading ensures that the graph will be animated when loaded
     if (data.length > 0 && width != 0) return (
         <>
-            <Typography variant="h5" color="primary">Your portfolio currently has ${currencyFormat(totalCash)}</Typography>
+            <Typography variant="h5" color="primary">Your portfolio currently has ${currencyFormat(totalPortfolioCash)}</Typography>
             <AreaChart width={width} height={400} data={data}>
                 <CartesianGrid strokeDasharray="1 1 " />
                 <XAxis dataKey="x" />
