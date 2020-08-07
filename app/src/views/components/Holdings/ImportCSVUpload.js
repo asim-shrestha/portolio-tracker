69ea98eb06173d13531ade40f87465a3dd31ef33
bmai <bmai@sfu.ca>
2020-08-07 16:11:46 -0700

    made csv upload look better

--- app/src/views/components/Holdings/ImportCSVUpload.js
+++ app/src/views/components/Holdings/ImportCSVUpload.js
@@ -1,23 +1,64 @@
-import React, { useState, useEffect } from 'react'
-import Typography from '@material-ui/core/Typography';
+import React, { useState, useEffect, useRef } from 'react'
+import Typography from '@material-ui/core/Typography'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
+import ListItemIcon from '@material-ui/core/ListItemIcon'
+import ArrowRightIcon from '@material-ui/icons/ArrowRight'
+import IconButton from '@material-ui/core/IconButton'
+import Button from '@material-ui/core/Button'
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
 
 export default ({ uploadHandler }) => {
 
+
+    // Create a reference to the hidden file input element
+    const hiddenFileInput = React.useRef(null)
+
+    // click hidden file input
+    const handleClick = event => {
+        hiddenFileInput.current.click();
+    };
+
     return (
         < div >
-            <Typography variant="h5" align="center" color="primary">
-                Upload a CSV file with the following columns:
-                    </Typography>
-            <ul>
-                <li>Symbol (example: TSLA, AMZN)</li>
-                <li>Price</li>
-                <li>Quantity</li>
-                <li>Date (YYYY-MM-DD)</li>
-            </ul>
+            <div style={{ padding: '0 25px 0 25px' }}>
+                <Typography variant="h6" align="center" color="primary">
+                    Please ensure the CSV file has the following columns:
+                </Typography>
 
+                <List dense disablePadding>
+                    <ListItem >
+                        <ListItemIcon><ArrowRightIcon /></ListItemIcon>
+                        <ListItemText primary="Symbol (example: TSLA, AMZN)" />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemIcon><ArrowRightIcon /></ListItemIcon>
+                        <ListItemText primary="Price" />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemIcon><ArrowRightIcon /></ListItemIcon>
+                        <ListItemText primary="Quantity" />
+                    </ListItem>
+                    <ListItem>
+                        <ListItemIcon><ArrowRightIcon /></ListItemIcon>
+                        <ListItemText primary="Date (YYYY-MM-DD)" />
+                    </ListItem>
+                </List>
+            </div>
+
+            <div style={{ textAlign: 'center' }}>
+                <Button onClick={handleClick} color="primary">
+                    <div>
+                        <CloudUploadIcon style={{ fontSize: '100px' }} />
+                        <Typography variant="h6">Upload</Typography>
+                    </div>
+                </Button>
+            </div>
             <form>
-                <input type="file" id="CSVUpload" accept=".csv" onChange={uploadHandler} />
+                <input ref={hiddenFileInput} onChange={uploadHandler} style={{ display: 'none' }} type="file" id="CSVUpload" accept=".csv" />
             </form>
+
         </div >
     )
 }
