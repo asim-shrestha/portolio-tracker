ac3ee0d448fd1120b8c970188ca6119536b824a0
Asim Shrestha <asim.shrestha@hotmail.com>
2020-08-10 15:40:17 -0700

    refactor: update seed stocks

--- app/src/db/seeds/02_activities.js.js
+++ app/src/db/seeds/02_activities.js.js
@@ -5,15 +5,15 @@ exports.seed = function (knex, Promise) {
             // Inserts seed entries
             return knex('activities').insert([
                 // For test@test.com
-                { user_id: 1, date: '2020-07-13', price: 1774.41, commission: 9.95, quantity: 75, action: 'buy', symbol: 'TSLA', },
-                { user_id: 1, date: '2020-06-26', price: 216.08, commission: 9.95, quantity: 50, action: 'buy', symbol: 'FB', },
+                { user_id: 1, date: '2020-07-13', price: 1774.41, commission: 9.95, quantity: 150, action: 'buy', symbol: 'TSLA', },
+                { user_id: 1, date: '2020-06-26', price: 216.08, commission: 9.95, quantity: 300, action: 'buy', symbol: 'FB', },
                 { user_id: 1, date: '2020-07-17', price: 230.17, commission: 9.95, quantity: 1337, action: 'buy', symbol: 'ZM', },
-                { user_id: 1, date: '2020-07-17', price: 55, commission: 9.95, quantity: 2000, action: 'buy', symbol: 'PTON', },
+                { user_id: 1, date: '2020-03-16', price: 322.17, commission: 9.95, quantity: 150, action: 'buy', symbol: 'SHOP', },
                 // For test@sfu.ca
-                { user_id: 2, date: '2020-07-13', price: 1774.41, commission: 9.95, quantity: 75, action: 'buy', symbol: 'TSLA', },
-                { user_id: 2, date: '2020-07-01', price: 200.08, commission: 9.95, quantity: 50, action: 'buy', symbol: 'FB', },
+                { user_id: 2, date: '2020-07-13', price: 1774.41, commission: 9.95, quantity: 150, action: 'buy', symbol: 'TSLA', },
+                { user_id: 2, date: '2020-07-01', price: 200.08, commission: 9.95, quantity: 300, action: 'buy', symbol: 'FB', },
                 { user_id: 2, date: '2020-07-17', price: 230.17, commission: 9.95, quantity: 1337, action: 'buy', symbol: 'ZM', },
-                { user_id: 2, date: '2020-07-17', price: 55, commission: 9.95, quantity: 2000, action: 'buy', symbol: 'PTON', },
+                { user_id: 2, date: '2020-03-16', price: 322.17, commission: 9.95, quantity: 150, action: 'buy', symbol: 'SHOP', },
             ]);
         });
 };
